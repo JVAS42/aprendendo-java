9032bbffca50292b6cf1cd1ffb178390a8d977d0
João Victor <61116930+JVAS42@users.noreply.github.com>
2023-10-30 08:00:14 -0300

    Questao 2.26
    Padrao de tabuleiro de damas de asteriscos

---
+++ exercicios-deitel/src/capitulo002/PadraoDeTabuleiroDeDamasDeAsteriscos.java
@@ -0,0 +1,10 @@
+package capitulo002;
+
+public class PadraoDeTabuleiroDeDamasDeAsteriscos {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 3; i++) {
+            System.out.println("* * * * * * * * ");
+            System.out.println(" * * * * * * * *");
+        }
+    }
+}
