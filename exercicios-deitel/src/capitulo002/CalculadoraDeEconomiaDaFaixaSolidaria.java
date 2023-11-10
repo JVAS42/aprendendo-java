c2dd57dc225a398bda51b1fdf45cd3d03cf41059
João Victor <61116930+JVAS42@users.noreply.github.com>
2023-11-10 09:07:36 -0300

    Questao 2.35
    Calculadora de economia da faixa solidaria
    Fiquei com duvida nessa questao, quem souber como faz me ajuda por favor

---
+++ exercicios-deitel/src/capitulo002/CalculadoraDeEconomiaDaFaixaSolidaria.java
@@ -0,0 +1,19 @@
+package capitulo002;
+
+import java.util.Scanner;
+
+public class CalculadoraDeEconomiaDaFaixaSolidaria {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double kmTotal, precoLitro, kmMedio, taxas, pedagio;
+
+        System.out.println("Informe o KM total, preco por liro, KM medio, taxas e pedagio: ");
+        kmTotal = input.nextDouble();
+        precoLitro = input.nextDouble();
+        kmMedio = input.nextDouble();
+        taxas = input.nextDouble();
+        pedagio = input.nextDouble();
+
+
+    }
+}
