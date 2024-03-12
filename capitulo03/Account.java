789c5f254c2ff00c7110c07e8db9f93a4264c68a
João Victor <61116930+JVAS42@users.noreply.github.com>
2024-03-12 20:28:36 -0300

    Questao 3.11

---
+++ capitulo03/Account.java
@@ -0,0 +1,44 @@
+package capitulo03;
+
+public class Account {
+    private String name;
+    private double balance;
+
+    public Account(String name, double balance) {
+        this.name = name;
+        if(balance > 0.0) {
+            this.balance = balance;
+        }
+    }
+
+    public void deposit(double depositAmount) {
+        if(depositAmount > 0.0) {
+            balance = balance + depositAmount;
+        }
+    }
+
+    //Classe criada para a questao 3.11
+    public boolean withdraw(double value) {
+        if(this.balance >= value) {
+            this.balance -= value;
+            System.out.println("Value withdraw: " + value);
+            System.out.println("Current balance: " + balance);
+            return true;
+        } else {
+            System.out.println("Withdrawal amount exceeded account balance");
+            return false;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
